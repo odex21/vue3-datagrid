196d9304af4e68e01f165e7b326e8b3e3e1af2f9
odex <odex_2100@outlook.com>
2021-05-06 05:44:44 +0800

    release: v0.0.8-fix.1

--- src/vgrid.ts
+++ src/vgrid.ts
@@ -1,8 +1,8 @@
 import { App, defineAsyncComponent } from 'vue'
 import * as loader from '@revolist/revogrid/custom-element'
 import { RevoGrid } from './revogrid'
-import vueTemplate, { vueTemplateConstructor } from './vue-template'
-import vueEditor from './vue-editor'
+// import vueTemplate, { vueTemplateConstructor } from './vue-template'
+// import vueEditor from './vue-editor'
 
 let isDefined = false
 
@@ -36,12 +36,12 @@ export const VGridPlugin = {
 }
 
 // Vue template wrapper for virtual nodes
-export const VGridVueTemplate = vueTemplate
+// export const VGridVueTemplate = vueTemplate
 
 // Vue adapter to convert vue component into VNode
-export const VGridVueTemplateConstructor = vueTemplateConstructor
+// export const VGridVueTemplateConstructor = vueTemplateConstructor
 
 // Vue editor wrapper
-export const VGridVueEditor = vueEditor
+// export const VGridVueEditor = vueEditor
 
 // export default VGrid;
